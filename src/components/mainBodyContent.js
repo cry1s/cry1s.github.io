da2cef81df2fe5520f7c016bf98b685a18feb469
markgregr <markgrevtsov@mail.ru>
2022-11-26 17:39:01 +0300

    try

--- src/components/mainBodyContent.js
+++ src/components/mainBodyContent.js
@@ -17,19 +17,26 @@ export default function MainBodyContent(props) {
     const [user, setUser] = useState(new User());
     const [curweek, setCurweek] = useState(1);
     const [curday, setCurday] = useState(0);
+    const handlerClickStrelkaLeft = () => {
+        if (curweek > 1) 
+            setCurweek(curweek-1);
+    }
+    const handlerClickStrelkaRight = () => {
+        setCurweek(curweek+1);
+    }
     useEffect(() => {
         setUser(props.user);
     }, [props.user]);
     return (
         <div>
             <div class="container bg-light justify-content-between rounded d-flex">
-                <button type="button" class="btn">
+                <button type="button" class="btn" onClick = {handlerClickStrelkaLeft}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-arrow-left-circle" viewBox="0 0 16 16">
                         <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"></path>
                     </svg>
                 </button>
                 <h3>{curweek} неделя</h3>
-                <button type="button" class="btn">
+                <button type="button" class="btn" onClick={handlerClickStrelkaRight}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-arrow-right-circle" viewBox="0 0 16 16">
                         <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"></path>
                     </svg>
