15ad00f8685caae8e8e10d381d1521c4ab7e5848
markgregr <markgrevtsov@mail.ru>
2022-11-27 16:00:28 +0300

    ok

--- src/components/mainBodyContent.js
+++ src/components/mainBodyContent.js
@@ -35,7 +35,7 @@ export default function MainBodyContent(props) {
                         <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"></path>
                     </svg>
                 </button>
-                <h3>{curweek} РЅРµРґРµР»СЏ</h3>
+                <h3>{curweek} неделя</h3>
                 <button type="button" class="btn" onClick={handlerClickStrelkaRight}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-arrow-right-circle" viewBox="0 0 16 16">
                         <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"></path>
@@ -44,13 +44,13 @@ export default function MainBodyContent(props) {
             </div>
             <div class="container">
                 <div class="row justify-content-center my-3">
-                    <button class="col-auto rounded-circle active" onClick={() => setCurday(0)}>РџРЅ</button>
-                    <button class="col-auto rounded-circle" onClick={() => setCurday(1)}>Р’С‚</button>
-                    <button class="col-auto rounded-circle" onClick={() => setCurday(2)}>РЎСЂ</button>
-                    <button class="col-auto rounded-circle" onClick={() => setCurday(3)}>Р§С‚</button>
-                    <button class="col-auto rounded-circle" onClick={() => setCurday(4)}>РџС‚</button>
-                    <button class="col-auto rounded-circle" onClick={() => setCurday(5)}>РЎР±</button>
-                    <button class="col-auto rounded-circle" onClick={() => setCurday(6)}>Р’СЃ</button>
+                    <button class="col-auto rounded-circle active" onClick={() => setCurday(0)}>Пн</button>
+                    <button class="col-auto rounded-circle" onClick={() => setCurday(1)}>Вт</button>
+                    <button class="col-auto rounded-circle" onClick={() => setCurday(2)}>Ср</button>
+                    <button class="col-auto rounded-circle" onClick={() => setCurday(3)}>Чт</button>
+                    <button class="col-auto rounded-circle" onClick={() => setCurday(4)}>Пт</button>
+                    <button class="col-auto rounded-circle" onClick={() => setCurday(5)}>Сб</button>
+                    <button class="col-auto rounded-circle" onClick={() => setCurday(6)}>Вс</button>
                 </div>
                 <SubjectsList subjects={props.user.table[curday + (props.user.twoweeks && (curweek % 2 == 0)) * 7]} curweek={curweek} curday={curday} deadlines={props.user.deadlines}
                 userindex={props.userindex} setuser={props.setuser}/>
