f7e5f094a5984ef38084cdb3bbaf2ee4337c6123
CAPVOK <capvok@yandex.ru>
2022-11-27 15:59:48 +0300

    v

--- src/components/deadline.js
+++ src/components/deadline.js
@@ -8,11 +8,11 @@ export default function Deadline(props){
     }, [props.deadline]);
     return (
         <div class="justify-content-center p-2">
-            <div class="p-2 bg-light shadow border rounded">
-                <h3>{deadline.name}</h3>
-                <p>Неделя - {deadline.week}</p>
-                <p>Дата - {deadline.day}</p>
-                <p>{deadline.description}</p>
+            <div class="p-2 bg-light shadow border  rounded">
+                <h3 class ="mb-0">{deadline.name}</h3>
+                <p class ="my-0">Неделя - {deadline.week}</p>
+                <p class ="mb-0">Дата - {deadline.day}</p>
+                <p class ="my-2">{deadline.description}</p>
             </div>
         </div>
     );
